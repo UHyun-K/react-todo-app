79a1413f1163ca602151a7624beb6b2bd284f3c5
UHyun-K <konglim2@naver.com>
2022-05-18 16:11:53 +0900

    category change

--- react-todo-app/src/components/ToDo.tsx
+++ react-todo-app/src/components/ToDo.tsx
@@ -1,13 +1,33 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+    const setToDo = useSetRecoilState(toDoState);
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget: { name },
+        } = event;
+    };
     return (
         <li>
             <span>{text}</span>
-            <button>ToDo</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>
+                    DOING
+                </button>
+            )}
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>
+                    TO_DO
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>
+                    DONE
+                </button>
+            )}
         </li>
     );
 }
+
 export default ToDo;
